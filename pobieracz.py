ab67a1daa96bcd524ae6b280448161d4f9289da3
root <root@retr0.home>
2020-12-06 20:21:47 +0100

    multi platformowość

--- pobieracz.py
+++ pobieracz.py
@@ -1,13 +1,10 @@
-import urllib.request
-import os
-def pobieracz(edzio, edzio2):
-    urllib.request.urlretrieve(a, b)
-    print("pobieranie zakończone!, plik ma rozmiar: ")
-    os.system(f"du -sh {b}")
+import urllib
+import os.path
 a = input("podaj strone z plikiem: ")
 x = a.split("/")
 b = input("nazwa pliku (domyślna enter): ")
 if b == "":
     b = x [-1]
-    print(b)
-pobieracz(a, b)
+urllib.request.urlretrieve(a, b)
+size = os.path.getsize(f'{b}')
+print("pobieranie zakończone!, plik ma rozmiar: ", size , "bajtów")
