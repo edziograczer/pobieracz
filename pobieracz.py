ae7e2545a0146998d343bfed99b9f6a27a577226
root <root@retr0.home>
2020-12-06 01:02:19 +0100

    wersja z defem

--- pobieracz.py
+++ pobieracz.py
@@ -1,8 +1,10 @@
 import urllib.request
 import os
+def pobieracz(edzio, edzio2):
+    urllib.request.urlretrieve(a, b)
+    print("pobieranie zakończone!, plik ma rozmiar: ")
+    os.system(f"du -sh {b}")
 a = input("podaj strone z plikiem: ")
 b = input("podaj nazwe lub patch do pobranego pliku: ")
-urllib.request.urlretrieve(a, b)
-print("pobieranie zakończone!, plik ma rozmiar: ")
-os.system(f"du -sh {b}")
+pobieracz(a, b)
 #tes
